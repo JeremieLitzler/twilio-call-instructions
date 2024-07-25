5cbe2d8e4e7216b46f40e6162fcd970b8e1eabeb
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-07-25 12:24:07 +0200

    try the AI suggestion for integrating <say-as> instead of <speak-as>

--- functions/twiml.js
+++ functions/twiml.js
@@ -7,7 +7,7 @@ exports.handler = async function(event, context) {
   const xmlResponse = `<?xml version="1.0" encoding="UTF-8"?>
     <Response>
     <Say>
-        The caller <speak-as interpret-as="telephone">${dynamicValue}</speak-as> has called the On-call team for a problem. A SMS was sent to you to confirm you're calling the person back. The message contains the caller's number. Thanks.
+        The caller <say-as interpret-as="telephone">${dynamicValue}</say-as> has called the On-call team for a problem. A SMS was sent to you to confirm you're calling the person back. The message contains the caller's number. Thanks.
     </Say>
 </Response>`;
 
